125d8993c77af40741c99906f547eafe33da832d
Johan <johan.kellen@linkoping.se>
2019-09-09 13:33:06 +0200

    exempel

---
+++ function-demos/functions-5.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Add two numbers
+ * @param int $val1
+ * @param int $val2
+ * @return int
+ */
+function addNumbers(int $val1, int $val2):int {
+    return $val1 + $val2;
+}
+
+$outside = addNumbers(42,22);
+
+
+
