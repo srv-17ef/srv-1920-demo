77ca1710dac3f3d3368232640c328e78338788af
Johan <johan.kellen@linkoping.se>
2019-10-01 17:31:13 +0200

    Create diagnos1.php

---
+++ diagnos/diagnos1.php
@@ -0,0 +1,25 @@
+<?php
+
+$bikes = [
+  [
+      "id"=>3,
+      "brand"=>"White",
+      "model"=>"SC 19 Low"
+  ],
+  [
+      "id"=>5,
+      "brand"=>"Merida",
+      "model"=>"xTreme"
+  ],
+  [
+      "id"=>76,
+      "brand"=>"Yosemite",
+      "model"=>"Lowstep 16"
+  ]
+];
+
+foreach($bikes as $bike){
+    echo $bike;
+}
+
+
